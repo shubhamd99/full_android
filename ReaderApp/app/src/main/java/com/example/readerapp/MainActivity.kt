bbe3da27a85fe6a072c6ee305c94f780619f8bd5
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2023-02-17 21:38:15 +0530

    hilt setup

--- ReaderApp/app/src/main/java/com/example/readerapp/MainActivity.kt
+++ ReaderApp/app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.readerapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -9,32 +8,19 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.readerapp.ui.theme.ReaderAppTheme
-import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            val db = FirebaseFirestore.getInstance()
-            val user: MutableMap<String, Any> = HashMap()
-            user["firstName"] = "Shubham"
-            user["lastName"] = "Dhage"
 
             ReaderAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-
-                    db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener {
-                            Log.d("FB", "onCreate: ${it.id}")
-                        }
-                        .addOnFailureListener {
-                            Log.d("FB", "onCreate: $it")
-                        }
                 }
             }
         }
