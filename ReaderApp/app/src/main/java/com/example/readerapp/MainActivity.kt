568419a37571b511ea356cb05f092e3804033fa5
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2023-02-17 21:33:21 +0530

    read app setup package

--- ReaderApp/app/src/main/java/com/example/readerapp/MainActivity.kt
+++ ReaderApp/app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -1,43 +1,50 @@
 package com.example.readerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.readerapp.ui.theme.ReaderAppTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val db = FirebaseFirestore.getInstance()
+            val user: MutableMap<String, Any> = HashMap()
+            user["firstName"] = "Shubham"
+            user["lastName"] = "Dhage"
+
             ReaderAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FB", "onCreate: ${it.id}")
+                        }
+                        .addOnFailureListener {
+                            Log.d("FB", "onCreate: $it")
+                        }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ReaderAppTheme {
-        Greeting("Android")
+
     }
 }
