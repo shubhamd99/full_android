b7205e81b3ef692028fb32d5393f1afdf48873c6
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2024-04-29 21:24:37 +0530

    theme switcher

---
+++ ThemeSwitcherComponent/README.md
@@ -0,0 +1,5 @@
+## Theme Switcher - Dark mode
+
+![img_url](https://i.imgur.com/zUee9Jz.png)
+
+![img_url](https://i.imgur.com/Y2TDWVw.png)
--- ThemeSwitcherComponent/app/src/main/java/com/example/themeswitchercomponent/MainActivity.kt
+++ ThemeSwitcherComponent/app/src/main/java/com/example/themeswitchercomponent/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.themeswitchercomponent.ui.screen.MainScreen
 import com.example.themeswitchercomponent.ui.theme.ThemeSwitcherComponentTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +16,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ThemeSwitcherComponentTheme {
-
+            var darkTheme by remember {
+                mutableStateOf(false)
+            }
+            ThemeSwitcherComponentTheme(darkTheme = darkTheme) {
+                MainScreen(
+                    darkTheme = darkTheme,
+                    onThemeUpdated = { darkTheme = !darkTheme }
+                )
             }
         }
     }
